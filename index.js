e698fe60f48f5dbcdac31aa5ee18fe3ef37669e8
Alabi Ayobami <arocket04@gmail.com>
2023-07-04 04:20:59 +0100

    using node-cron now

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 console.log('the code has started!!!!!!');
 const fetchNews = require('./fetchNews');
 const storeNews = require('./storeNews');
+console.log('successful imports of externals!!!!!!');
 
-const countries = ["us", "cn", "in", "br", "id", "pk", "ng", "bd", "ru", "jp", "mx", "ph", "vn", "eg", "de", "ir", "tr", "gb", "fr", "it", "za", "kr", "es", "ar", "ca", "au", "co", "my", "th", "sa", "pe", "nl", "cl", "se", "be", "ch", "pt", "pl", "sg", "hk", "dk", "fi", "no", "ie", "at", "nz", "gr", "il", "cz", "ro", "ke", "ma", "et", "dz", "gh", "tz", "ci"];
+
+// Run the function once a day
+
+const cron = require('node-cron');
+
+const countries =  ["us", "cn", "in", "br", "id", "pk", "ng", "bd", "ru", "jp", "mx", "ph", "vn", "eg", "de", "ir", "tr", "gb", "fr", "it", "za", "kr", "es", "ar", "ca", "au", "co", "my", "th", "sa", "pe", "nl", "cl", "se", "be", "ch", "pt", "pl", "sg", "hk", "dk", "fi", "no", "ie", "at", "nz", "gr", "il", "cz", "ro", "ke", "ma", "et", "dz", "gh", "tz", "ci"
+  // Add the 50 most internet active country codes here, including Nigeria ('ng')
+];
 
 async function fetchAndStoreNews() {
   for (const country of countries) {
-    const articles = await fetchNews(country, 'climate'); // Change 'technology' to the desired category
+    const articles = await fetchNews(country, 'climate'); 
+    console.log('it has fetched the news!')// Change 'technology' to the desired category
     await storeNews(country, articles);
-    console.log('successful at first');
   }
 }
 
-// Run the function once a day
-setInterval(fetchAndStoreNews, 24 * 60 * 60 * 1000);
+// Run the function at a specific time every day. In this case at midnight.
+cron.schedule("30 4 * * *", function() {
+  console.log("---------------------");
+  console.log("Running Cron Job");
+  fetchAndStoreNews();
+});
