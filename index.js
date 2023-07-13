9714be18db9fdd376389565ac31bb1c9b3d063e1
Alabi Ayobami <arocket04@gmail.com>
2023-07-13 13:19:16 +0100

    refined wih claude

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const fetchNews = require('./fetchNews');
 const storeNews = require('./storeNews');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; 
 
-let lastCronJob1Run = '';
+// Use the PORT environment variable for the port instead of a hardcoded 3000
+async function fetchAndStoreNews() {
+  for (const country of countries) {
+    const articles = await fetchNews(country, 'Climate');
+    console.log('it has fetched the news!');
+    await storeNews(country, articles);
+  }
+}
+
+let lastCronJob1Run = ''; 
 let lastCronJob2Run = '';
 
 // Define your cron jobs
@@ -23,30 +32,8 @@ cron.schedule("06 00 * * *", function() {
   fetchAndStoreNews();
 });
 
-// Define the route to display the last run time of cron jobs
-
-
-// Function to fetch and store news
-
-async function fetchAndStoreNews() {
-  for (const country of countries) {
-    const articles = await fetchNews(country, 'Climate');
-    console.log('it has fetched the news!');
-    await storeNews(country, articles);
-  }
-}
-
-
-app.get('/', (req, res) => {
-  res.send(`
-    <h1>Last Cron Job Run Times</h1>
-    <p>Cron Job 1: ${lastCronJob1Run}</p>
-    <p>Cron Job 2: ${lastCronJob2Run}</p>
-  `);
-});
-
-// Start the server
+// ...
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`); 
 });
