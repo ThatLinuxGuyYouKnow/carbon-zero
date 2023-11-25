30c58d2dc2108c6bda3f7f6447f4616c7c883f16
Alabi Ayobami <92615787+ThatLinuxGuyYouKnow@users.noreply.github.com>
2023-11-25 23:47:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const cron = require('node-cron');
+const cron = require('node-cron')
 const fetchNews = require('./fetchNews');
 const storeNews = require('./storeNews');
 const winston = require('winston');
